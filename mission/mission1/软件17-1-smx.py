03b91e45df768a024005f8c8f197c6ebd3109385
sunmaoxiang <sunmaoxiang98@163.com>
2018-11-10 21:49:40 +0800

    子任务2完成-命令行可传入多个文件目录

--- mission/mission1/软件17-1-smx.py
+++ mission/mission1/软件17-1-smx.py
@@ -1,5 +1,5 @@
-# test
 import os
+import sys
 
 def dfs_print(path, depth):
     dirnames = os.listdir
@@ -13,15 +13,19 @@ def dfs_print(path, depth):
         if os.path.isfile(newpath):
             print('-' * (depth * 2) + ' ' + item)
     
-
-
 def traverse_directory(name):
     filecnt = 0
-    for dirpath, dirnames, filenames in os.walk(name):
-        filecnt += len(filenames)
-    dfs_print(name, 1)
-    print("\n\n文件数 %d" % filecnt)
-
+    try:
+        for dirpath, dirnames, filenames in os.walk(name):
+            filecnt += len(filenames)
+        dfs_print(name, 1)
+    except Exception as e:
+        print("Not Exist!")
+        return
+    print("\n文件数 %d\n===========================" % filecnt)
 
 if __name__ == '__main__':
-    traverse_directory("C:\MyRepository\TEST")
+    print("===========================")
+    for i in sys.argv[1:]:
+        print("文件夹名：", i) 
+        traverse_directory(i)
